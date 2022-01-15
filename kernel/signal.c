4fd61c9eb3c93f2ac8fdbb507f7b2f648588da8e
Amit Daniel Kachhap <amit.kachhap@arm.com>
2022-01-15 19:08:16 +0530

    kernel: signal: Modify task_struct.sas_ss_sp to user_uintptr_t
    task_struct.sas_ss_sp is used to store the user stack pointer provided
    by the sigaltstack syscall. This commit will help in setting up the correct
    signal frame and not lose the capability during its use.
    
    Also make use of the new user_ptr_is_same() helper to ensure we do
    not ignore an update to ss_sp that leaves the address unchanged.
    
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>
    Co-developed-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -4234,8 +4234,7 @@ do_sigaltstack (const stack_t *ss, stack_t *oss, unsigned long sp,
 
 	if (oss) {
 		memset(oss, 0, sizeof(stack_t));
-		/* TODO [PCuABI] - preserving user-provided capability */
-		oss->ss_sp = uaddr_to_user_ptr(t->sas_ss_sp);
+		oss->ss_sp = (void __user *)t->sas_ss_sp;
 		oss->ss_size = t->sas_ss_size;
 		oss->ss_flags = sas_ss_flags(sp) |
 			(current->sas_ss_flags & SS_FLAG_BITS);
@@ -4259,7 +4258,7 @@ do_sigaltstack (const stack_t *ss, stack_t *oss, unsigned long sp,
 		 * Return before taking any locks if no actual
 		 * sigaltstack changes were requested.
 		 */
-		if (t->sas_ss_sp == user_ptr_addr(ss_sp) &&
+		if (user_ptr_is_same((void __user *)t->sas_ss_sp, ss_sp) &&
 		    t->sas_ss_size == ss_size &&
 		    t->sas_ss_flags == ss_flags)
 			return 0;
@@ -4275,8 +4274,7 @@ do_sigaltstack (const stack_t *ss, stack_t *oss, unsigned long sp,
 				ret = -ENOMEM;
 		}
 		if (!ret) {
-			/* TODO [PCuABI] - preserving user-provided capability */
-			t->sas_ss_sp = user_ptr_addr(ss_sp);
+			t->sas_ss_sp = (user_uintptr_t)ss_sp;
 			t->sas_ss_size = ss_size;
 			t->sas_ss_flags = ss_flags;
 		}
@@ -4319,7 +4317,7 @@ int restore_altstack(const stack_t __user *uss)
 int __save_altstack(stack_t __user *uss, unsigned long sp)
 {
 	struct task_struct *t = current;
-	int err = __put_user_ptr(uaddr_to_user_ptr(t->sas_ss_sp), &uss->ss_sp) |
+	int err = __put_user_ptr((void __user *)t->sas_ss_sp, &uss->ss_sp) |
 		__put_user(t->sas_ss_flags, &uss->ss_flags) |
 		__put_user(t->sas_ss_size, &uss->ss_size);
 	return err;
