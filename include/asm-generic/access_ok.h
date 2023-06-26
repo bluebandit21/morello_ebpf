c260dc9fbc9063130e62f8a66deb32b29a86a7ff
Kevin Brodsky <kevin.brodsky@arm.com>
2023-06-26 12:58:35 +0100

    uaccess: Fix user pointer downcast
    Do not cast a user pointer directly to a type that cannot hold a
    CHERI capability, use user_ptr_addr() to extract its address.
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- include/asm-generic/access_ok.h
+++ include/asm-generic/access_ok.h
@@ -31,7 +31,7 @@
 static inline int __access_ok(const void __user *ptr, unsigned long size)
 {
 	unsigned long limit = TASK_SIZE_MAX;
-	unsigned long addr = (unsigned long)ptr;
+	unsigned long addr = user_ptr_addr(ptr);
 
 	if (IS_ENABLED(CONFIG_ALTERNATE_USER_ADDRESS_SPACE) ||
 	    !IS_ENABLED(CONFIG_MMU))
